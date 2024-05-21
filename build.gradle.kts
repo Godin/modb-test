b4ee8db81ccc488b7314ffdc6381391553f1b76c
Evgeny Mandrikov <mandrikov@gmail.com>
2024-05-21 22:00:25 +0200

    (WIP)

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,11 +50,13 @@ tasks.withType<Test> {
 }
 
 tasks.jacocoTestReport {
+    /*
     reports {
         html.required.set(false)
         xml.required.set(true)
         xml.outputLocation.set(file("${layout.buildDirectory}/reports/jacoco/test/jacocoFullReport.xml"))
     }
+    */
     dependsOn(allprojects.map { it.tasks.named<Test>("test") })
 }
 
